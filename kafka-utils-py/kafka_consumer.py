e9ea00d8c6c5dd6e9bf5d452d4437d1bed8001a6
Fernando Nos <fernando_nos@sicredi.com.br>
2021-06-10 14:21:16 -0300

    including files

---
+++ kafka-utils-py/kafka_consumer.py
@@ -0,0 +1,33 @@
+from kafka import KafkaConsumer
+from confluent_kafka import Consumer, KafkaError
+
+def config_kafka_consumer():
+    return {
+        'bootstrap.servers': 'dev-events-broker-id-1.dev-sicredi.in:9092',
+        'group.id': 'test-1234',
+        'enable.auto.commit': True,
+        'session.timeout.ms': 6000,
+    }
+
+
+def init_kafka_consumer(configs):
+
+    try:
+        c = Consumer(configs)
+        print("Connected!")
+        c.subscribe(['conecta-manual-notification-message'])
+        print("Subscribed!")
+        while True:
+            msg = c.poll(0.1)
+            if msg is None:
+                continue
+            elif not msg.error():
+                print('Received message: {0}'.format(msg.value()))
+            elif msg.error().code() == KafkaError._PARTITION_EOF:
+                print('End of partition reached {0}/{1}'
+                      .format(msg.topic(), msg.partition()))
+            else:
+                print('Error occured: {0}'.format(msg.error().str()))
+
+    except KeyboardInterrupt:
+        return
