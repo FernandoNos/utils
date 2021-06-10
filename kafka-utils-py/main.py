a946a1bf6fa9bde1147fabf98a25f43bedf93297
Fernando Nos <fernando_nos@sicredi.com.br>
2021-06-10 13:55:03 -0300

    integrating with kafka

---
+++ kafka-utils-py/main.py
@@ -0,0 +1,20 @@
+from kafka_consumer import *
+
+if __name__ == '__main__':
+
+    while True:
+        print("\n ############################# \n")
+        option = input("1. consume from topics\n"
+                       "2. write to topic\n"
+                       "3. add new configuration\n"
+                       "Choose an action: ")
+        if option == '1':
+            configs = config_kafka_consumer()
+            init_kafka_consumer(configs)
+        elif option == '2':
+            print("TODO WRITER")
+        elif option == '3':
+            print("TODO NEW CONFIG")
+        else:
+            print("Unknown option!")
+    print("Finished")
