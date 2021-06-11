93f9da2b40bb5fac9d53c285b75dcaa26d91d486
Fernando Nos <fernando_nos@sicredi.com.br>
2021-06-11 16:29:06 -0300

    selecting topic from list to produce to

--- kafka-utils-py/kafka_producer.py
+++ kafka-utils-py/kafka_producer.py
@@ -1,14 +1,30 @@
 from confluent_kafka import Producer
-from kafka_config import get_config
+from kafka_config import *
 
 
 def send_messages():
     try:
         host = get_config('host')
+        topic = select_topic()
         while True:
             message = input("Type in your message: ")
             p = Producer({'bootstrap.servers': host})
-            p.produce('conecta-manual-notification-message', value=message)
+            p.produce(topic, value=message)
             p.flush(30)
     except KeyboardInterrupt:
         return
+
+
+def select_topic():
+    topics = get_topics()
+    menu = "Select one topic: \n"
+
+    index = 0
+    for topic in topics:
+        menu += "{0}. {1}\n".format(str(index), topic)
+        index += 1
+
+    menu += "Index: "
+
+    selected_index = int(input(menu))
+    return topics[selected_index]
